59d85f83c47de3b0ed83668f784bdbe435167f85
Ankit Kumar Thakur <103780825+DrThakur@users.noreply.github.com>
2023-01-01 22:06:28 +0530

    Drum kit files uploaded

---
+++ index.js
@@ -0,0 +1,61 @@
+// alert("Welcome!");
+let numOfButtons = document.querySelectorAll(".drum").length;
+for (let i = 0; i < numOfButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    //  console.log(this.innerHTML); //using "this" will get me the object on which event listner is applied.
+    let buttonInnerHtml = this.innerHTML;
+    handleKeyPress(buttonInnerHtml);
+    buttonAnimation(buttonInnerHtml);
+  });
+}
+
+document.addEventListener("keydown", function (event) {
+  //   alert("A key was pressed.");
+  handleKeyPress(event.key);
+  buttonAnimation(event.key);
+});
+
+function handleKeyPress(key) {
+  switch (key) {
+    case "w":
+      let tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      let tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      let tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      let tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      let crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "k":
+      let kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    case "l":
+      let snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    default:
+      console.log(buttonInnerHtml);
+      break;
+  }
+}
+
+function buttonAnimation(currentKey) {
+  let activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
